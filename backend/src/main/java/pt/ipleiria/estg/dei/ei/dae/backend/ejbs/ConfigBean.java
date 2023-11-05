091874e9e683ee7eaf7101fa2663b330f3887b50
Paulo Clara <pjclara@gmail.com>
2023-11-05 19:11:52 +0000

    Inicio do projecto

---
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/ejbs/ConfigBean.java
@@ -0,0 +1,13 @@
+package pt.ipleiria.estg.dei.ei.dae.backend.ejbs;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+@Startup
+@Singleton
+public class ConfigBean {
+    @PostConstruct
+    public void populateDB() {
+        System.out.println("Hello Java EE!");
+    }
+}
