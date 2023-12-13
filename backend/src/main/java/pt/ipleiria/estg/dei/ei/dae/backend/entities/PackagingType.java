ab1486b8935acaaf0fa22735f7b1cd0f640106c8
Mary Pereira <marianarp27@gmail.com>
2023-12-13 22:28:41 +0000

    add name to ENUM of PackageType

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/PackagingType.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/PackagingType.java
@@ -1,8 +1,19 @@
 package pt.ipleiria.estg.dei.ei.dae.backend.entities;
 
 public enum PackagingType {
-    PRIMARY,
-    SECONDARY,
-    THIRD,
-    TRANSPORT
+    PRIMARY("Primary"),
+    SECONDARY("Secondary"),
+    THIRD("Trird"),
+    TRANSPORT("Transport");
+
+
+    private final String nome;
+
+    PackagingType(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
