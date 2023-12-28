3e021de3234705a3048c98423cda62a5010cf2d0
LuisZMarques <MarquesPK@gmail.com>
2023-12-28 21:07:20 +0000

    cart modal

---
+++ frontend/components/CardModal.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-dialog
+        v-model="cartModal"
+        width="500"
+    >
+    asdasd
+    </v-dialog>
+</template>
+
+<script setup>
+const props = defineProps({
+  modalOpen: Boolean
+})
+
+const cartModal = props.modalOpen;
+
+</script>
