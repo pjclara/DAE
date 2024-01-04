bfa2e79b41d44027054a5989b54f883e157e71db
LuisZMarques <MarquesPK@gmail.com>
2024-01-04 22:36:51 +0000

    sensor update folder test commit

---
+++ sensors-update/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-app-bar color="deep-purple">
+      <v-col align="center">
+        <v-row>
+          <v-app-bar-title justify="center">Atualização de Sensores</v-app-bar-title>
+        </v-row>
+      </v-col>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
