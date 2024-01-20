136bcd97091f9fecc83eff69ae55b9efaa9674c6
LuisZMarques <MarquesPK@gmail.com>
2024-01-20 23:38:08 +0000

    update da app sensors

--- sensors-update/src/App.vue
+++ sensors-update/src/App.vue
@@ -12,18 +12,16 @@
         <v-col cols="6" align="center">
           <v-select label="Selecione o tipo de embalagem" v-model="selectedPackageType" :items="packageType"></v-select>
 
-          <v-select label="Selecione a Encomenda" v-if="selectedPackageType == 'Orders'" v-model="selectedOrder"
-            :items="orders" item-title="id" item-value="id"></v-select>
-
+          <v-select label="Selecione a Encomenda" v-if="selectedPackageType == 'Orders'" v-model="orderId" :items="orders"
+            item-title="endConsumerName" item-value="id"></v-select>
           <v-select label="Selecione o tipo de Produto" v-if="selectedPackageType == 'Product'" v-model="selectedProduct"
             :items="products" item-title="name" item-value="id"></v-select>
 
           <v-select label="Selecione o Produto Unitário" v-if="selectedProduct" v-model="selectedUnitProduct"
             :items="unitaryProducts" item-title="serialNumber" item-value="id"></v-select>
-{{ unitaryProductsList }}
-          <v-select label="Selecione o sensor do Produto Unitário" v-if="sensorsData" :items="sensorsData"
-            item-title="sensorName" item-value="sensorId" v-model="idSensor"></v-select>
 
+          <v-select label="Selecione o sensor" v-if="sensorsData" :items="sensorsData"
+            item-title="sensorName" item-value="sensorId" v-model="idSensor"></v-select>
           <v-text-field v-model="sensorValue" label="Insira valor"></v-text-field>
 
           <v-btn color="primary" @click="updateSensorValue" v-if="sensorValue">Atualizar</v-btn>
@@ -36,6 +34,7 @@
 
 <script setup>
 import fetch from 'node-fetch';
+import { resolveTransitionHooks } from 'vue';
 import { ref, watch } from "vue";
 
 const packageType = ref(['Product', 'Orders'])
@@ -54,11 +53,14 @@ const selectedProduct = ref(null)
 const unitaryProducts = ref([])
 
 const unitaryProductsList = ref(null)
+
 const selectedUnitProduct = ref(null)
 
 const sensorsData = ref([])
 
 const idSensor = ref(null)
+
+const orderId = ref(null)
 
 // const sensors = ref([])
 // const selectedSensor = ref(null)
@@ -71,7 +73,11 @@ const sensorValue = ref(null)
 
 watch(() => selectedPackageType.value,
   async (newPackageType) => {
-    //console.log("newPackageType:", newPackageType);
+    selectedOrder.value = null;
+    selectedProduct.value = null;
+    selectedUnitProduct.value = null;
+    sensorsData.value = [];
+
     if (newPackageType == 'Product') {
       await fetchAllProducts();
     } else {
@@ -92,12 +98,37 @@ async function fetchAllOrders() {
 
     const data = await response.json();
     if (data) {
-      //console.log("data: ", data)
+      console.log("data: ", data)
       data.forEach(element => {
         orders.value.push({
           data: element,
           id: element.id,
           endConsumerName: element.endConsumerName,
+        })
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+  }
+}
+
+async function fetchOrder(id) {
+  try {
+    const response = await fetch(`http://localhost:8080/backend/api/orders/${id}`, {
+      method: 'get',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    if (data) {
+      console.log("data: ", data)
+      data.packageSensorDTO.sensorValueDTOS.forEach(element => {
+        sensorsData.value.push({
+          sensorId: element.id,
+          sensorName: element.sensorDTO.type,
+          sensorValue: element.value
         })
       });
     }
@@ -125,9 +156,9 @@ async function fetchAllProducts() {
     const data = await response.json();
 
     if (data) {
-      //console.log("data: ", data)
+      console.log("data: ", data)
       products.value = data;
-      //console.log("products.value: ", products.value)
+      console.log("products.value: ", products.value)
     }
   } catch (error) {
     console.error("Error fetching products:", error);
@@ -136,13 +167,36 @@ async function fetchAllProducts() {
 
 watch(() => selectedProduct.value,
   async (newSelectedProduct) => {
-    //console.log("newSelectedProduct:", newSelectedProduct);
+    console.log("newSelectedProduct:", newSelectedProduct);
     if (newSelectedProduct) {
-      //console.log("newSelectedProduct: ", newSelectedProduct);
+      console.log("newSelectedProduct: ", newSelectedProduct);
       await fetchUnitaryProducts(newSelectedProduct);
     }
   }
 );
+
+watch(() => orderId.value,
+  async (newOrderId) => {
+    console.log("newOrderId:", newOrderId);
+    if (newOrderId) {
+      console.log("newOrderId: ", newOrderId);
+      await fetchOrder(newOrderId);
+    }
+  }
+);
+
+watch(() => idSensor.value,
+  async (sensorValue) => {
+    sensorsData.value.forEach(element => {
+      if (element.sensorId == idSensor.value) {
+        if(element.sensorValue != null){
+          alert("Sensor já com o valor: " + element.sensorValue)
+      }
+      }
+    });
+  }
+);
+
 
 async function fetchUnitaryProducts() {
   try {
@@ -156,16 +210,18 @@ async function fetchUnitaryProducts() {
     const data = await response.json();
 
     if (data) {
-      //console.log("data: ", data)
+      console.log("data: ", data)
       unitaryProductsList.value = [];
-        if(!element.available)
-       { unitaryProductsList.value.push({
-          data: element,
-          id: element.id,
-          serialNumber: element.serialNumber,
-        })}
+      data.forEach(element => {
+        if (!element.available) {
+          unitaryProductsList.value.push({
+            id: element.id,
+            serialNumber: element.serialNumber,
+          })
+        }
+      });
       unitaryProducts.value = data;
-      //console.log("unitaryProducts.value: ", unitaryProducts.value)
+      console.log("unitaryProducts.value: ", unitaryProducts.value)
     }
   } catch (error) {
     console.error("Error fetching unitaryProducts:", error);
@@ -174,15 +230,15 @@ async function fetchUnitaryProducts() {
 
 watch(() => selectedUnitProduct.value,
   async (newSelectedUnitProduct) => {
-    //console.log("newSelectedUnitProduct:", newSelectedUnitProduct);
+    console.log("newSelectedUnitProduct:", newSelectedUnitProduct);
     if (newSelectedUnitProduct) {
-      //console.log("newSelectedUnitProduct: ", newSelectedUnitProduct);
-      await fetchSensors(newSelectedUnitProduct);
+      console.log("newSelectedUnitProduct: ", newSelectedUnitProduct);
+      await fetchSensorsUniproduct(newSelectedUnitProduct);
     }
   }
 );
 
-async function fetchSensors(id) {
+async function fetchSensorsUniproduct(id) {
   try {
     const response = await fetch(`http://localhost:8080/backend/api/unitProducts/${id}`, {
       method: 'get',
@@ -194,10 +250,10 @@ async function fetchSensors(id) {
     const data = await response.json();
 
     if (data) {
-      //console.log("data: ", data)
+      console.log("data: ", data)
       sensorsData.value = [];
       sensorValue.value = null;
-      //console.log("sensorsData.value: ", data.packageSensorDTO.sensorValueDTOS)
+      console.log("sensorsData.value: ", data.packageSensorDTO.sensorValueDTOS)
       data.packageSensorDTO.sensorValueDTOS.forEach(element => {
         sensorsData.value.push({
           sensorId: element.id,
@@ -212,6 +268,11 @@ async function fetchSensors(id) {
 }
 
 async function updateSensorValue() {
+  if (!idSensor.value) {
+    alert("Selecione um sensor")
+    return;
+  }
+
   try {
     const response = await fetch(`http://localhost:8080/backend/api/sensorValues/${idSensor.value}/updateSensorValue/${sensorValue.value}`, {
       method: 'put',
@@ -223,10 +284,12 @@ async function updateSensorValue() {
     });
 
     const data = await response.json();
-    //console.log("data: ", data)
+    console.log("data: ", data)
 
-    if (data) {   
-      alert("Valor atualizado com sucesso!")   
+    if (data) {
+      alert("Valor atualizado com sucesso!")
+      // relload page
+      location.reload();
     }
   } catch (error) {
     console.error("Error fetching unitaryProducts:", error);
